d30edf45f0f5630ab4f621b3b9d7bb02e6d9425a
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-07-18 08:57:04 -0700

    Update lc.py

--- lc.py
+++ lc.py
@@ -46,3 +46,24 @@ class Solution:
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2 
+#Invert Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+            stack = [root]
+            while stack:
+                node = stack.pop()
+                if node:
+                    node.left, node.right = node.right, node.left
+                    stack.extend([node.right, node.left])
+            return root
+        """
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
