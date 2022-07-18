57aa29dfd73c84e849cd5600c77778026546d6e1
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-07-18 07:52:46 -0700

    Update lc.py

--- lc.py
+++ lc.py
@@ -1,1 +1,11 @@
 
+#Two sum:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dic:
+                return [i,dic[target - nums[i]]]
+            dic[nums[i]] = i
+  
+#
