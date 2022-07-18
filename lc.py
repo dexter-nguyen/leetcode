371151dd7a8c1103e16508e062c6e272d92aacff
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-07-18 08:01:29 -0700

    Update lc.py

--- lc.py
+++ lc.py
@@ -8,4 +8,12 @@ class Solution:
                 return [i,dic[target - nums[i]]]
             dic[nums[i]] = i
   
-#
+#Best Time to Buy and Sell Stock
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
