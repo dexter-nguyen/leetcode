9fe8012b34c527e9d7961dc36bdb6335519a2303
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-07-18 08:35:08 -0700

    Update lc.py

--- lc.py
+++ lc.py
@@ -34,3 +34,15 @@ class Solution:
             elif len(stack) == 0 or d[stack.pop()] != i:  # 2
                 return False
         return len(stack) == 0 # 3
+#Merge Two Sorted Lists
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    
+        if not list1 or not list2:
+            return list1 or list2
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2 
