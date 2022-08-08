c0093b7d5c383ca5be1c7d22dd4fb24652f07e07
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-07 20:22:11 -0700

    Implement Queue with stack

--- lc.py
+++ lc.py
@@ -85,5 +85,26 @@ class Solution:
             mid = low + (high - low)//2
             
         return low
-                 
+
+ class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        return self.stack.pop(0)
+        
+
+    def peek(self) -> int:
+        return self.stack[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+            
         
