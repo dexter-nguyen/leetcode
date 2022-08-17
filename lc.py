010a21c6e25579d1a369d750f5a17b52d0405dc0
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-16 17:29:52 -0700

    Roman to Int

--- lc.py
+++ lc.py
@@ -1,4 +1,18 @@
 class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = { 'I':1, 'V':5,'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        
+        s = s.replace("IV","IIII").replace("IX","VIIII")
+        s = s.replace("XL","XXXX").replace("XC","LXXXX")
+        s = s.replace("CD","CCCC").replace("CM","DCCCC")
+        
+        result = 0
+        
+        for char in s:
+            result += dic[char]
+            
+        return result
+class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         dic = Counter(nums)
         
