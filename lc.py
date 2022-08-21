f48f8a72f83b5aff579b20664545ac8d6af6e588
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-21 13:52:20 -0700

    Meeting room

--- lc.py
+++ lc.py
@@ -1,3 +1,17 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+        
+        intervals.sort()
+
+        for i in range(1, len(intervals)):
+            if intervals[i-1][1] > intervals[i][0]:
+                return False
+            
+        return True
+    
+        # time = nlogn and O(1) 
  import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
