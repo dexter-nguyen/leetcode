0d6b29ac7ba924e5fb9eeb47a1df00b2ceb294f1
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-21 14:44:20 -0700

    Min climbing stairs

--- lc.py
+++ lc.py
@@ -1,4 +1,23 @@
 class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        if not cost:
+            return 0
+        
+        dp = [0] * len(cost) 
+        
+        dp[0] = cost[0]
+        
+        if len(cost) > 1:
+            dp[1] = cost[1]
+            
+        for i in range(2,len(cost)):
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2])
+            
+        return min(dp[-1],dp[-2])
+            
+            
+class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         if not intervals:
             return True
