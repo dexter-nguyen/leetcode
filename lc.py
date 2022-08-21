f6f50ad74372d0e05ddc38d790ca694a045bbd84
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-21 13:26:36 -0700

    k-largest

--- lc.py
+++ lc.py
@@ -1,3 +1,30 @@
+ # minHeap => first element of len k is the kth- largest
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums[:min(k, len(nums))]
+        
+        heapify(self.heap)
+        for i in range(k,len(nums)):
+            heappushpop(self.heap,nums[i])       
+            
+        print(self.heap)
+
+    def add(self, val: int) -> int:
+        heappush(self.heap,val)
+        if len(self.heap) > self.k:
+            heappop(self.heap)
+         
+        print(self.heap)
+        return self.heap[0]
+            
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
