16b8b3d6516b97cb74c6cade79645e0bc6d94c02
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-23 10:32:13 -0700

    Maximum Subarray

--- lc.py
+++ lc.py
@@ -1,4 +1,21 @@
 class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        result = nums[0]
+        if len(nums) == 1:
+            return nums[0]
+        for i in range(1,len(nums)):
+            if nums[i-1] + nums[i] > nums[i]:
+                nums[i] = nums[i-1] + nums[i] 
+            if result <= nums[i]:
+                result = nums[i]
+                
+        return result
+                
+        
+            
+        
+        
+class Solution:
     def longestPalindrome(self, s: str) -> str:
         
         res = ""
