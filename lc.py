41431e5dae00dd6abeb6c2183648daa960deb504
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-22 23:48:26 -0700

    House Robber 2

--- lc.py
+++ lc.py
@@ -1,6 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        #
+        def house_robber(nums):
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2,len(nums)):
+                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+            return dp[-1]
+
+        if len(nums) <=2 : 
+            return max(nums)
+        return max( house_robber(nums[1:]), house_robber(nums[:-1]) )
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
         nums = [0,0,0] +  nums
         
         for i in range(3,len(nums)):
