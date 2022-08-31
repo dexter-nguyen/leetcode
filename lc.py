dcbefa6969c95d94b941a476455299b95c996c10
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-08-31 11:21:36 -0700

    LIS: n^2

--- lc.py
+++ lc.py
@@ -1,4 +1,20 @@
-#dp sections
+
+/class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        #maintaining curmax and cur increasing
+        if len(nums) == 1:
+            return 1
+        dp = [1] * len(nums)
+        
+        
+        maxx = nums[0]
+        for i in range(1,len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] =  max(dp[i], dp[j] +1)
+                
+        return  max(dp)
+        #dp sections
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:
         dic = defaultdict(str)
