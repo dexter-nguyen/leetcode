e46393dac775830d0d1717843961933a6b5cd6ba
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-10-08 20:18:53 -0700

    Populating the next right pointers

--- lc.py
+++ lc.py
@@ -1,3 +1,18 @@
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        queue = deque()
+        if root:
+            queue.append(root)
+        while queue:
+            node = queue.popleft()
+            if node.left and node.right:
+                queue.append(node.left)
+                queue.append(node.right)
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+        return root
 def strongPasswordCheckerII( password: str) -> bool:
     
     n = len(password)
