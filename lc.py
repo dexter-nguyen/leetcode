69a5262ac867e9ed131cd86d248399226e34454e
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-10-17 11:13:11 -0700

    Add Two Numbers

--- lc.py
+++ lc.py
@@ -1,3 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        result = head
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = (l1.val if l1 else 0)
+            v2 = (l2.val if l2 else 0)
+            value = v1 + v2 + carry
+            if value > 9:
+                value= value % 10
+                carry = 1
+            else:
+                carry = 0
+           
+            result.next = ListNode(value)
+            result = result.next
+                
+            if l1:
+                l1 = l1.next
+            if l2: 
+                l2 = l2.next
+          
+        return head.next     
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         start = []
