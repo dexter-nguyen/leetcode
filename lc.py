ff248ca8ce7a006a4efcee47f0ef2bbf77241646
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2022-10-19 09:45:39 -0700

    1169. Invalid Transaction

--- lc.py
+++ lc.py
@@ -1,4 +1,54 @@
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        """
+        :type transactions: List[str]
+        :rtype: List[str]
+        """
+        
+        r = {}
+                
+        inv = []        
 
+        for i in transactions:
+            name,time,amount,city = i.split(",")
+            time = int(time)
+            amount = int(amount)
+            
+            if time not in r:
+                r[time] = {
+                    name: [city]
+                }
+            else:
+                if name not in r[time]:
+                    r[time][name]=[city]
+                else:
+                    r[time][name].append(city)
+        print(r)
+        
+        for i in transactions:
+            name,time,amount,city = i.split(",")
+            time = int(time)
+            amount = int(amount)
+            
+
+            if amount > 1000:
+                inv.append(i)
+                continue
+            
+            for j in range(time-60, time+61):
+                if j not in r:
+                    continue
+                if name not in r[j]:
+                    continue
+                if len(r[j][name]) > 1 or (r[j][name][0] != city):
+                    inv.append(i)
+                    break
+                                        
+        return inv 
+
+
+
+        
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
 
