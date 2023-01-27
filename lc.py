118654e402a03beb3418fa4ba2845a6e368d40cb
Tran Nguyen <58058227+dexternguyen56@users.noreply.github.com>
2023-01-26 20:03:12 -0800

    787. Cheapest Flights Within K Stops

--- lc.py
+++ lc.py
@@ -1,4 +1,26 @@
 class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        #bfs
+        graph, dis = collections.defaultdict(list), [-1 for _ in range(n)]
+        for f, to, price in flights:
+            graph[f].append([to, price])
+
+        dis[src], q, step = 0, collections.deque([src]), 0
+
+        while q and step <= k:
+            sz = len(q)
+            new_dis = list(dis)
+            for _ in range(sz):
+                cur = q.popleft()
+                for neighbor in graph[cur]:
+                    if new_dis[neighbor[0]] == -1 or new_dis[neighbor[0]] > dis[cur]+neighbor[1]:
+                        new_dis[neighbor[0]] = dis[cur] + neighbor[1]
+                        q.append(neighbor[0])
+            step += 1
+            dis = new_dis
+
+        return dis[dst]
+class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         def bin_search(target):
             l=0
